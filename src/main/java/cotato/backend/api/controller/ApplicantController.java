d4794ee0d516a7c5e0d9b3b7960370aa21f894b7
hyunj24 <hyunjug7@naver.com>
2025-11-05 20:24:04 +0900

    fix: ApplicationResponse 추가 후 사용

--- src/main/java/cotato/backend/api/controller/ApplicantController.java
+++ src/main/java/cotato/backend/api/controller/ApplicantController.java
@@ -2,6 +2,7 @@ package cotato.backend.api.controller;
 
 import cotato.backend.domain.example.application.ApplicantService;
 import cotato.backend.domain.example.dto.request.UpdateApplicantRequest;
+import cotato.backend.domain.example.dto.response.ApplicantResponse;
 import cotato.backend.domain.example.entity.Applicant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,9 @@ public class ApplicantController {
 
     // 지원자 단건 조회 반환
     @GetMapping("/{id}")
-    public ResponseEntity<Applicant> get(@PathVariable Long id) {
-        return ResponseEntity.ok(applicantService.getById(id));
+    public ResponseEntity<ApplicantResponse> getById(@PathVariable Long id) {
+        ApplicantResponse response = applicantService.getByIdDto(id);
+        return ResponseEntity.ok(response);
     }
 
     // 제공된 필드만 부분 수정
